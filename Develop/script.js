6e8a458d5b40e7fcd6281faf314fe0abcc77c26e
Chris McNamara <cmcnamara1991@icloud.com>
2023-02-26 20:12:26 -0600

    Added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Wondering if there was a way to write an array without typing out each letter I found 
+// that there was. Using Array.from and choosing a length I was able to reference numeric 
+// places for those characters from unicode character chart and pull them into an Array!
 var numbers = Array.from({length: 10}, (_, i) => String.fromCharCode(48 + i));
 console.log(numbers)
 
@@ -10,6 +13,8 @@ console.log(uppercase);
 var lowercase = Array.from({length: 26}, (_, i) => String.fromCharCode(97 + i));
 console.log(lowercase);
 
+// that method proved to be difficult for special characters because they are not 
+// organized sequently on the chart. So I decided to type them out
 var specialChar = [
   "!", "@", "#", "$", "%", "^", "&","*", "(", ")", "_", "-", "+", "=", 
   "~", "`", "{", "}", "[", "]", "|", ":", ";", "<", ">", "?", ".", ",",
@@ -17,14 +22,16 @@ var specialChar = [
 console.log(specialChar)
 
 var allChars;
-// numbers.concat(uppercase, lowercase, specialChar);
+// ^It took me a long time to understand that I needed to declare this variable completely empty
 
 
+// the function to select a random element 
 function selectRandomElement(characters) {
   var index = Math.floor(Math.random() * characters.length) 
   return characters[index];
 }
 
+// the function that prompts the user for information and adjusts the array accordingly
 function generateCharacterOptions() {
   allChars = []; 
   var confirmNumbers = window.confirm("Do you want numbers in your password?");
@@ -40,27 +47,32 @@ function generateCharacterOptions() {
   var affirmSpecialChar = window.confirm("Do you want special characters in your Password?");
   if(affirmSpecialChar){
   allChars = allChars.concat(specialChar);}
-  else {}
+  else {
+    alert("Please choose at least one character set")
+  }
   return allChars;
 }
 
 function generatePassword() {
   console.log("clicked the button")
-  var userinput = prompt("Please enter a number between 8 and 128");
-  if (userinput < 8) {
-   alert("Please select proper length")}
-  else if (userinput > 128) {
-   alert("Please select proper length")}
+  var userinfo = prompt("Please enter a number between 8 and 128");
+  if(userinfo < 8) {
+  alert("Please enter a proper length")}
+  else if (userinfo > 128) {
+  alert("Please select proper length")}
+  // else if (userinfo != Number){
+  // alert("Please enter a number")}
   
   else {
-    userinput = Number(userinput);
+    userinfo = Number(userinfo);
     var affirm = generateCharacterOptions();
     console.log(affirm)
     if(affirm) {
       var password =''
-      for(var i=0; i < userinput; i++) {
+      for(var i=0; i < userinfo; i++) {
       password += selectRandomElement(allChars)
      }
+    // using a for loop to select elements of the array based on users input 
     }
     console.log(password)
     return password;
@@ -76,21 +88,5 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); 
 
-
-// ___Comments for line 3___
-// Wondering if there was a way to write an array without typing out each letter I found 
-// that there was. Using Array.from and choosing a length I was able to reference numeric 
-// places for those characters from unicode character chart and pull them into an Array!
-
-// ___Comments for line 14____
-// that method proved to be difficult for special characters because they are not 
-// organized sequently on the chart. So I decided to type them out
-
-// ___Comments for line 18___
-// console.log(allChar)--- Using the .concat method I combined these arrays globally/
-
-// ___Comments for line 21___
-// After much struggle trying to use .sort to randomize the array  decided to create a function with 
-// hope that it could be reusable.
 
 // if (userinput >= 8 && userinput <= 136)
